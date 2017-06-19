b1e20fdc7fc4e48ff38a8e513f1b1b33caf69969
Martin Hochel <hochelmartin@gmail.com>
2017-06-20 01:22:15 +0200

    step-4: add complete GameStateBar type coverage

--- src/app/GameStateBar.tsx
+++ src/app/GameStateBar.tsx
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 
-export class GameStateBar extends Component<any, any> {
-  state = { gameState: '' };
+type State = {
+  gameState: '' | 'X Wins!' | 'O Wins!' | 'Draw';
+};
 
-  handleGameStateChange(e) {
+export class GameStateBar extends Component<{}, State> {
+  state = { gameState: '' } as State;
+
+  handleGameStateChange(e: CustomEvent) {
     this.setState({ gameState: e.detail });
   }
 
-  handleRestart(e) {
+  handleRestart(e: Event) {
     this.setState({ gameState: '' });
   }
 
   componentDidMount() {
-    window.addEventListener('gameStateChange', e => this.handleGameStateChange(e));
-    window.addEventListener('restart', e => this.handleRestart(e));
+    window.addEventListener('gameStateChange', (e: CustomEvent) => this.handleGameStateChange(e));
+    window.addEventListener('restart', (e: CustomEvent) => this.handleRestart(e));
   }
 
   componentWillUnmount() {
-    window.removeEventListener('gameStateChange', e => this.handleGameStateChange(e));
-    window.removeEventListener('restart', e => this.handleRestart(e));
+    window.removeEventListener('gameStateChange', (e: CustomEvent) => this.handleGameStateChange(e));
+    window.removeEventListener('restart', (e: CustomEvent) => this.handleRestart(e));
   }
 
   render() {
